ba569a9e531a6439926bb7602b1c14e5e43457db
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-07 18:47:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,14 +5,15 @@
 
 </head>
 <body>
+    <p>hey</p>    
 <?php
-require 'vendor/autoload.php';
+/*require 'vendor/autoload.php';
 $client = \Symfony\Component\Panther\Client::createChromeClient();
 //use Goutte\Client;
 //$client = new Client();
 $crawler = $client->request('GET', 'https://www.youtube.com/results?search_query=despacito');
 sleep(3);
-$client->clickLink('Luis Fonsi - Despacito ft. Daddy Yankee');
+$client->clickLink('Luis Fonsi - Despacito ft. Daddy Yankee');*/
 ?>
 </body>
 </head>
